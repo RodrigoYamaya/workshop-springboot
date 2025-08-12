be3ccb4be6851c3d0c6532e4d06ef0e12fef02ee
rodrigo <kakakpegado@hotmail.com>
2025-08-11 22:20:45 -0300

    feat(service): Implementa a camada de serviço para Order

--- src/main/java/com/RodriSolution/course/service/OrderService.java
+++ src/main/java/com/RodriSolution/course/service/OrderService.java
@@ -1,4 +1,26 @@
 package com.RodriSolution.course.service;
 
+import com.RodriSolution.course.exceptions.RecursoNaoEncontrado;
+import com.RodriSolution.course.model.entities.Order;
+import com.RodriSolution.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class OrderService {
+    @Autowired
+    OrderRepository orderRepository;
+
+    public List<Order> OrdersAll() {
+        return orderRepository.findAll();
+
+    }
+
+    public Order findById(Long id) {
+        return orderRepository.findById(id)
+                .orElseThrow(()-> new RecursoNaoEncontrado("Pedido com ID " + id + " não encontrado."));
+
+    }
 }
