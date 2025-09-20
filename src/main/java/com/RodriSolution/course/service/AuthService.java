c574c29bdcdd9595f4cdade0efe9256ece680bdc
rodrigo <kakakpegado@hotmail.com>
2025-09-19 23:12:01 -0300

    feat(auth): implementar AuthService com login e geração de JWT

--- src/main/java/com/RodriSolution/course/service/AuthService.java
+++ src/main/java/com/RodriSolution/course/service/AuthService.java
@@ -1,9 +1,39 @@
 package com.RodriSolution.course.service;
 
+import com.RodriSolution.course.model.dtos.auth.LoginRequestDto;
+import com.RodriSolution.course.model.dtos.auth.LoginResponseDto;
+import com.RodriSolution.course.model.entities.User;
+import com.RodriSolution.course.repositories.UserRepository;
+import com.RodriSolution.course.security.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
 
 @RequiredArgsConstructor
 @Service
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
+    public LoginResponseDto login(LoginRequestDto dto) {
+        User user = userRepository.findByEmail(dto.email())
+                .orElseThrow(() -> new IllegalArgumentException("Email ou senha invalido"));
+
+        //verifaremos senha//
+        if(!passwordEncoder.matches(dto.password(), user.getPassword())) {
+            throw new IllegalArgumentException("Senha invalida ou email");
+        }
+
+        //gerar token jwt
+        String token = jwtService.generateToken(user);
+
+        //calcular experação
+        Instant expiresAt = jwtService.generateExpirationDate();
+
+        return new LoginResponseDto(token, user.getUserRole().name(), expiresAt);
+    }
 }
