02a569cecdc6cb93799c2f56ec9acf5ff326d9c7
rodrigo <kakakpegado@hotmail.com>
2025-09-22 21:26:17 -0300

    feat: Adiciona filtro JWT para autenticação

--- src/main/java/com/RodriSolution/course/security/JwtAuthenticationFilter.java
+++ src/main/java/com/RodriSolution/course/security/JwtAuthenticationFilter.java
@@ -1,4 +1,69 @@
 package com.RodriSolution.course.security;
 
-public class JwtAuthenticationFilter {
+import com.RodriSolution.course.model.entities.User;
+import com.RodriSolution.course.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public JwtAuthenticationFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        //Extrairemos token no header Authorization
+        final String authHeader = request.getHeader("Authorization");
+        if(authHeader == null || authHeader.startsWith("Bearer ")) {
+           filterChain.doFilter(request,response);
+           return;
+        }
+
+        final String token = authHeader.substring(7);
+
+        //validaremos token
+        if(!jwtService.isTokenValid(token)) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //buscaremos usuario pelo ID no token
+        Long userId = jwtService.getUserIdFromToken(token);
+        User user    = userRepository.findById(userId).orElse(null);
+        if(user == null) {
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //criaremos autenticação no spring security
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                user,null,
+                Collections.singletonList(()-> "ROLE_" + user.getUserRole().name()));
+
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        //setar contextp de autenticação
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+        //continuar o filtro
+        filterChain.doFilter(request,response);
+    }
 }
--- src/main/java/com/RodriSolution/course/security/SecurityConfig.java
+++ src/main/java/com/RodriSolution/course/security/SecurityConfig.java
@@ -1,21 +1,33 @@
 package com.RodriSolution.course.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/category/**").permitAll()
@@ -23,8 +35,17 @@ public class SecurityConfig {
                         .requestMatchers("/product/**").permitAll()
                         .requestMatchers("/user/**").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 
